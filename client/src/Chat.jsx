971cef7eff6028e28f717689b642048a83df5498
E062004 <131036712+E062004@users.noreply.github.com>
2025-04-10 00:54:25 +0530

    Update Chat.jsx

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -19,7 +19,10 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('wss://chat-app-backend-b7tm.onrender.com');
+    //const ws = new WebSocket('wss://chat-app-backend-b7tm.onrender.com');
+    const ws = new WebSocket('wss://chat-app-backend-b7tm.onrender.com', {
+  withCredentials: true
+});
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
